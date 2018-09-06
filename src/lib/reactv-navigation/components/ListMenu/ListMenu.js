44529c0e0ec3767e41f56eb2232592472ce92af7
Tomoya32 <tomoyamiyake32@gmail.com>
2018-09-05 23:42:25 -0600

    implemented simple element.scrollIntoView() on home page. Converted ListMenu to a class component in order to make use of lifecycle methods

--- src/lib/reactv-navigation/components/ListMenu/ListMenu.js
+++ src/lib/reactv-navigation/components/ListMenu/ListMenu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import './ListMenu.css'
@@ -18,13 +19,49 @@ class Renderer extends React.Component {
   }
 }
 
-const ListMenu = ({data = [], renderItem, className, index, focused, horizontal}) => (
-  <div className={cx('ListMenu', className, {horizontal})}>
-    {data.map((item, currentIndex) => {
-      return <Renderer key={currentIndex} item={item} focused={ focused && currentIndex === index} render={renderItem}/>
-    })}
-  </div>
-)
+class ListMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.listItem = React.createRef();
+  }
+  render() {
+    const { data, renderItem, className, index, focused, horizontal } = this.props;
+    return (
+      <div className={cx('ListMenu', className, {horizontal})}>
+        {data.map( (item,currentIndex) => {
+          let active = (focused && currentIndex === index);
+          let props = {
+            key: currentIndex,
+            item: item,
+            focused: active,
+            render: renderItem
+          };
+          if (active) { props.ref = this.listItem; }
+          return <Renderer {...props} />
+        })}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.scrollElementIntoViewIfNeeded();
+  }
+
+  componentDidUpdate() {
+    this.scrollElementIntoViewIfNeeded();
+  }
+
+  scrollElementIntoViewIfNeeded() {
+    let node = ReactDOM.findDOMNode(this.listItem.current);
+    if (node) {
+      // TODO: configure position of element
+      // see Web API: https://developer.mozilla.org/en-US/docs/Web/API/Element
+      node.scrollIntoView();
+      // TODO: fix width of element before enabling smooth scrolling below
+      // itemComponent.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
 /*
     <ListMenu data={data} renderItem={ListCard} horizontal focused menuid='single-list-menu' />
  */
