9205f64957c7a39a094f8aa52be0622db990d563
Tomoya32 <tomoyamiyake32@gmail.com>
2018-09-27 11:27:20 -0500

    Removed bash scripts and calling from Dockerfile.

--- scripts/create_env_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -e .env ]; then
-    echo ".env file already exists"
-else
-    echo "REACT_APP_AMAZON_MUSIC_CLIENT_ID=$REACT_APP_AMAZON_MUSIC_CLIENT_ID" > .env
-    echo "REACT_APP_SERIAL_NUMBER=$REACT_APP_SERIAL_NUMBER" >> .env
-    echo "REACT_APP_MUSIC_ENDPOINT=$REACT_APP_MUSIC_ENDPOINT" >> .env
-fi
