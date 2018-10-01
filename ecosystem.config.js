f5d9f3ef417d24cb684ed5702b2a9b6cbc727dcb
Tomoya32 <tomoyamiyake32@gmail.com>
2018-10-01 13:39:03 -0700

    Saving local storage stuff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,11 @@ module.exports = {
       },
       env_mike : {
         NODE_ENV: 'development',
-        PM2_SERVE_PORT: 8084
+        REACT_APP_AMAZON_MUSIC_CLIENT_ID: 'amzn1.application-oa2-client.364cc7e64af04ec5b96bbbdb985a4df2',
+        REACT_APP_AMAZON_CODE_CLIENT_ID: 'amzn1.application-oa2-client.3b25f0dda98045dfa0d99cb5721ce8d4',
+        REACT_APP_SERIAL_NUMBER: 'mikerocks',
+        REACT_APP_MUSIC_ENDPOINT: 'http://amazon-x1-proxy.weade.co/api',
+        REACT_APP_AUTH_ENDPOINT: 'http://amazon-x1-proxy.weade.co/auth/auth/O2/',
       }
     }
   ],
@@ -24,7 +28,7 @@ module.exports = {
   deploy : {
     mike : {
       user : 'deploybot',
-      host : 'doorkey-staging.weade.co',
+      host : 'doorkey.weade.co',
       ref  : 'origin/mike',
       repo : 'git@github.com:adiffengine/amazon-music-x1.git',
       path : '/var/www/apps/amazon-x1-mike',
