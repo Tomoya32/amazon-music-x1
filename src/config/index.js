b67f2e4fc26d3c297b172f5dbca7da7ad918391d
Tomoya32 <tomoyamiyake32@gmail.com>
2018-10-04 10:44:35 -0700

    Saving some fixes to the proxies. Made this a lot simpler

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   linking: {
     client_id: process.env.REACT_APP_AMAZON_MUSIC_CLIENT_ID,
-    code_id: process.env.REACT_APP_AMAZON_MUSIC_CLIENT_ID,
+    code_id: process.env.REACT_APP_AMAZON_CODE_CLIENT_ID,
     serial_number: process.env.REACT_APP_SERIAL_NUMBER,
     scope: 'amazon_music:access'
   },
