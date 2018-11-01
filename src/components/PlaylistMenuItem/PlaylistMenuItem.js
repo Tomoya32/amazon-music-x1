175bc9583518be4bacc19abab0fbedefb7468148
Tomoya32 <tomoyamiyake32@gmail.com>
2018-11-01 00:00:02 -0600

    Added left play icon on highlighted track and adjusted "Now Playing" menu item.

--- src/components/PlaylistMenuItem/PlaylistMenuItem.js
+++ src/components/PlaylistMenuItem/PlaylistMenuItem.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import cx from 'classnames'
 import css from './PlaylistMenuItem.scss'
+import PlayIcon from '../../assets/images/icon/play-icon.js'
 
 const PlaylistMenuItem = ({item: {itemLabel, image, subtitle}, focused}) => (
   <div className={cx(css.PlaylistMenuItem, {focused})}>
-    {image && <img src={image.uri} height={60} width={60} alt={itemLabel} className='image' /> }
+    {
+      focused ? <div style={{'margin-right': '25px'}}><PlayIcon fill={'#fff'} /></div>
+        : image && <img src={image.uri} height={60} width={60} alt={itemLabel} className='image' />
+    }
     <div className='info'>
       <label className='title'>{itemLabel}</label>
       <label className='subtitle'>{subtitle}</label>
