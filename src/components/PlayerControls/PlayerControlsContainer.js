8942fab29fe96ff9a54004742fe5031777212a88
tomoya <tomoyamiyake32@gmail.com>
2018-11-05 03:48:22 -0500

    Updated the PlayerControlsContainer

--- src/components/PlayerControls/PlayerControlsContainer.js
+++ src/components/PlayerControls/PlayerControlsContainer.js
@@ -8,6 +8,7 @@ import { bindActionCreators } from 'redux'
 import { push, replace} from '../../store/modules/nav'
 import { withRouter } from 'react-router'
 import { mergeChunkWithPathAndQuery, getLocation } from '../../lib/utils'
+import { loadChildNode } from '../../store/modules/music'
 
 import { setCurrentTime, setPlayerState, updateInitOnUpdate } from '../../store/modules/player'
 import { getTrackContainerChunkDescription } from '../../pages/Playback/selectors'
@@ -47,6 +48,7 @@ const mapDispatchToProps = (dispatch) => {
     setCurrentTime,
     setPlayerState,
     setPlayable,
+    loadChildNode,
     push, replace,
     sendThumbs
   }, dispatch)
@@ -144,6 +146,7 @@ class PlayerControlsContainer extends Component {
   handleTrackPlayback(direction) {
     const { setPlayable, location, playable, chunk, shuffle, music } = this.props
     const node = location.pathname.replace(/^\/?playback\/*/, '/').replace(/\/$/, '')
+    this.props.loadChildNode(node)
     const musicPlayable = location.hash
     const lastTrackChunk = chunk['trackInstances'].length - 1
     const indexTrackChunk = parseInt(playable.indexWithinChunk)
@@ -153,8 +156,9 @@ class PlayerControlsContainer extends Component {
     } else {
       if (indexTrackChunk >= 0) {
         if (direction === 1) {
-          if (lastTrackChunk >= (indexTrackChunk + 1))
+          if (lastTrackChunk >= (indexTrackChunk + 1)) {
             setPlayable(node, musicPlayable, (indexTrackChunk + 1).toString())
+          }
           else {
             if(chunk['nextTrackPointer']) {
               const totalURL = getLocation(music.pathResolvers[playable.node])
@@ -181,10 +185,12 @@ class PlayerControlsContainer extends Component {
   }
 
   backwardSkip () {
-    this.reset()
     const { currentTime } = this.props
     if (currentTime > 2) this.restart()
-    else this.handleTrackPlayback(0)
+    else {
+      this.reset()
+      this.handleTrackPlayback(0)
+    }
   }
 
   forwardSkip () {
